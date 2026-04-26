88cf0e7ec38f8b2a0d256d44ee6d045dae8195c3
stephanoignacio28-del <stephanoignacio28@outlook.com>
2026-04-26 12:17:23 -0500

    agregado el ejercicio 1

---
+++ EJERCICIO1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+num3 = int(input("Ingrese el tercer numero: "))
+
+promedio = (num1 + num2 + num3) / 3
+
+print(f"El promedio es: {promedio}")
